449b81a30dfe728ba97a2247b1259f5007c82eef
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-08-30 11:04:31 +0200

    Refactor eventlistener

--- js/script.js
+++ js/script.js
@@ -25,26 +25,16 @@ nextButton.addEventListener('click',
         if (activeIndex < images.length - 1) {
             carouselContainer.innerHTML = '';
             activeIndex++;
-
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'w-100 active' : 'w-100';
-
-                carouselContainer.innerHTML += `<img class="${imageClass}" src="images/${image}.jpg" alt="img-${image}">`;
-            }
         } else if (activeIndex === images.length - 1) {
             activeIndex = 0;
-            console.log('after index ' + activeIndex);
-
             carouselContainer.innerHTML = '';
+        }
 
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'w-100 active' : 'w-100';
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            const imageClass = i === activeIndex ? 'w-100 active' : 'w-100';
 
-                carouselContainer.innerHTML += `<img class="${imageClass}" src="images/${image}.jpg" alt="img-${image}">`;
-            }
-
+            carouselContainer.innerHTML += `<img class="${imageClass}" src="images/${image}.jpg" alt="img-${image}">`;
         }
 
     })
@@ -52,32 +42,21 @@ nextButton.addEventListener('click',
 previousButton.addEventListener('click',
     function () {
         console.log('click');
-        console.log('before index ' + activeIndex);
 
         if (activeIndex > 0) {
             carouselContainer.innerHTML = '';
             activeIndex--;
-
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'w-100 active' : 'w-100';
-
-                carouselContainer.innerHTML += `<img class="${imageClass}" src="images/${image}.jpg" alt="img-${image}">`;
-            }
         } else if (activeIndex === 0) {
             activeIndex = images.length;
-            console.log('after index ' + activeIndex);
-
             carouselContainer.innerHTML = '';
             activeIndex--;
+        }
 
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'w-100 active' : 'w-100';
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            const imageClass = i === activeIndex ? 'w-100 active' : 'w-100';
 
-                carouselContainer.innerHTML += `<img class="${imageClass}" src="images/${image}.jpg" alt="img-${image}">`;
-            }
-
+            carouselContainer.innerHTML += `<img class="${imageClass}" src="images/${image}.jpg" alt="img-${image}">`;
         }
 
     })
